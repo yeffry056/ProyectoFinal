af5bffa3eadaa8b686be422d20968c078ccc6498
Danilo Gabriel Bonifacio Bautista <danilo_bonifacio@ucne.edu.do>
2021-11-14 18:49:04 -0400

    Registro Empleado Totalmente Completado

---
+++ UI/Consultas/cEmpleado.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ProyectoFinal.BLL;
+using ProyectoFinal.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cEmpleado.xaml
+    /// </summary>
+    public partial class cEmpleado : Window
+    {
+        public cEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Empleados>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //EmpleadoId
+                        listado = EmpleadoBLL.GetList(e => e.EmpleadoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1: //Nombre                      
+                        listado = EmpleadoBLL.GetList(e => e.Nombre.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = EmpleadoBLL.GetList(c => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = EmpleadoBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = EmpleadoBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            // listado = UsuariosBLL.GetList();
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
